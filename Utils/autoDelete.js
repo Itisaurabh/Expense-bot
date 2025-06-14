92393c7942e5ca0776af0848b8320fe7dfffd6ce
Itisaurabh <75170240+Itisaurabh@users.noreply.github.com>
2025-06-14 07:49:02 +0530

    Delete Utils directory

--- Utils/autoDelete.js
+++
@@ -1,9 +0,0 @@
-export async function replyAndDelete(interaction, content) {
-  const reply = await interaction.reply({ content, ephemeral: false }); // always non-ephemeral
-  setTimeout(async () => {
-    try {
-      const msg = await interaction.fetchReply();
-      if (msg.deletable) await msg.delete();
-    } catch {}
-  }, 60000); // delete after 60 seconds
-}
